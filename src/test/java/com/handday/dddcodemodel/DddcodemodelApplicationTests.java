88b6cd8181f85875594d075a5e8a800d03185c45
向海青 <xh91900@126.com>
2021-07-12 11:33:15 +0800

    first commit

---
+++ src/test/java/com/handday/dddcodemodel/DddcodemodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handday.dddcodemodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddcodemodelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
